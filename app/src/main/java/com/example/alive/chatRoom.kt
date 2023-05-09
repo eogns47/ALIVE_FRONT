de7a3a8f0f20c0101d3df75974d22a991bd42f58
eogns47 <eogns47@naver.com>
2023-05-09 20:40:04 +0900

    리셋

--- app/src/main/java/com/example/alive/chatRoom.kt
+++ app/src/main/java/com/example/alive/chatRoom.kt
@@ -15,30 +15,10 @@ class chatRoom : AppCompatActivity() {
         binding=ActivityChatRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        binding.toolbar.title="툴바 타이틀"
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu, menu)
-        return true
-    }
 
-    //액션버튼 클릭 했을 때
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item?.itemId){
-            R.id.action_back -> {
-                //검색 버튼 눌렀을 때
-                Toast.makeText(applicationContext, "검색 이벤트 실행", Toast.LENGTH_LONG).show()
-                return super.onOptionsItemSelected(item)
-            }
-            R.id.action_menu_divider -> {
-                //공유 버튼 눌렀을 때
-                Toast.makeText(applicationContext, "공유 이벤트 실행", Toast.LENGTH_LONG).show()
-                return super.onOptionsItemSelected(item)
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
+
+
 }
